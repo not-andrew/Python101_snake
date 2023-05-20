68a41ed034af47a1d4c55add203dd43b73b5b1fe
AndyBanescu <130507451+AndyBanescu@users.noreply.github.com>
2023-05-20 17:21:39 +0300

    Update Score.py

--- Score.py
+++ Score.py
@@ -7,13 +7,4 @@ def show_score(score_color, background_color, font, size, screen, score):
     score_background = pygame.Surface((score_surface.get_width(),score_surface.get_height()))
     score_background.fill(background_color)
     screen.blit(score_background, (posx, posy))
-    screen.blit(score_surface, score_obj)import pygame
-def show_score(score_color, background_color, font, size, screen, score):
-    score_font = pygame.font.SysFont(font,size)
-    score_surface = score_font.render('Score: ' + str(score), True, score_color)
-    score_obj = score_surface.get_rect()
-    print (score_surface.get_height())
-    score_background = pygame.Surface((score_surface.get_width(),score_surface.get_height()))
-    score_background.fill(background_color)
-    screen.blit(score_background, (posx, posy))
     screen.blit(score_surface, score_obj)
