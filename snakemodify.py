56df38ed223424a18f45ce89309e58984abfdd0c
AndyBanescu <130507451+AndyBanescu@users.noreply.github.com>
2023-05-20 15:38:01 +0300

    Update snakemodify.py

--- snakemodify.py
+++ snakemodify.py
@@ -1,11 +1,13 @@
 import pygame
 import random
 def snake_modify(snake_body,snake_position, fruit_position, score, fruit_spawn, screen, green):
-
+    print (snake_position)
+    print(fruit_position)
     snake_body.insert(0, list(snake_position))
-    if snake_position[0] == fruit_position[0] and snake_position[1] == fruit_position[1]:
+    if snake_position[0] <= fruit_position[0] + 5 and snake_position[0] >= fruit_position[0] - 5 and snake_position[1] <= fruit_position[1] + 5 and snake_position[1] >= fruit_position[1] - 5:
         score = score + 10
         fruit_spawn = False
+        print("DA")
         
     else:
         snake_body.pop()
