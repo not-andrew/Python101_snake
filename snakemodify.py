844713ad0bf1405a97bf6c1ca589c7fed5281ce4
AndyBanescu <130507451+AndyBanescu@users.noreply.github.com>
2023-05-20 17:43:43 +0300

    Update snakemodify.py

--- snakemodify.py
+++ snakemodify.py
@@ -2,6 +2,9 @@ import pygame
 import random
 def snake_modify(snake_body,snake_position, fruit_position, score, fruit_spawn, screen, green):
     snake_body.insert(0, list(snake_position))
+    if fruit_spawn == False:
+        fruit_position = [random.randrange(1, (720//10)) * 10,
+                          random.randrange(1, (480//10)) * 10]
     if snake_position[0] <= fruit_position[0] + 5 and snake_position[0] >= fruit_position[0] - 5 and snake_position[1] <= fruit_position[1] + 5 and snake_position[1] >= fruit_position[1] - 5:
         score = score + 10
         fruit_spawn = False
@@ -9,8 +12,7 @@ def snake_modify(snake_body,snake_position, fruit_position, score, fruit_spawn, 
         
     else:
         snake_body.pop()
-    
-    if not fruit_spawn:
+    if fruit_spawn == False:
         fruit_position = [random.randrange(1, (720//10)) * 10,
                           random.randrange(1, (480//10)) * 10]
         
@@ -19,6 +21,6 @@ def snake_modify(snake_body,snake_position, fruit_position, score, fruit_spawn, 
     
     for pos in snake_body:
         pygame.draw.rect( screen, green, pygame.Rect(pos[0],pos[1],10,10))
-    pygame.draw.rect(screen, green, pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
+    pygame.draw.rect(screen, pygame.Color(255,0,0), pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
 
     return snake_body,snake_position, fruit_position, score, fruit_spawn, screen, green
