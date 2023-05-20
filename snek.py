d510402b80f0c157fce36817733cfe1a54e3e3d5
not-andrew <andreiiancu105@gmail.com>
2023-05-20 14:00:10 +0300

    first steps to implement walls

--- snek.py
+++ snek.py
@@ -1,19 +1,32 @@
 import pygame
+import random
+import wall
 
+screenWidth=720
+screenHeight=480
 # pygame setup
 pygame.init()
 pygame.display.set_caption('Python101 Snake')
-screen = pygame.display.set_mode((720, 480))
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 fps = pygame.time.Clock()
 running = True
 
+snakeSpeed=3
 snake_position = [100, 50]
 direction = 'RIGHT'
 nextDirection="RIGHT"
+
+
+#culori
+wallColor=pygame.Color(255,0,180)
 green = pygame.Color(0, 255, 0)
 
 
-direction="RIGHT"
+
+def drawWall(wall, startPosition):
+    #draw all the squares in position as described in the wall variation
+    for square in wall:
+        pygame.draw.rect(screen, wallColor, pygame.Rect(startPosition[0] + 10*square[0], startPosition[1] + 10*square[1], 10,10))
 
 def readKey():
     global running
@@ -45,6 +58,34 @@ def setDirection():
     if nextDirection=="LEFT" and direction!="RIGHT":
         direction=  "LEFT"
 
+def updateSnakePosition():
+    global direction
+    global snake_position
+    #updatare pozitie sarpe spre directia de deplasare
+    if direction=="RIGHT":
+        snake_position[0]+=snakeSpeed
+    if direction=="LEFT":
+        snake_position[0]-=snakeSpeed
+    if direction=="UP":
+        snake_position[1]-=snakeSpeed
+    if direction=="DOWN":
+        snake_position[1]+=snakeSpeed
+
+    #daca sarpele trece de limita ecranului se reintoarce in cealalta parte a ecranului
+    #verificare pe axa X
+    if snake_position[0]<0:
+        snake_position[0]=screenWidth+snake_position[0]
+    if snake_position[0]>screenWidth:
+        snake_position[0]=snake_position[0]-screenWidth
+    #verificare pe axa Y
+    if snake_position[1]<0:
+        snake_position[1]=screenHeight+snake_position[1]
+    if snake_position[1]>screenHeight:
+        snake_position[1]=snake_position[1]-screenHeight
+
+
+
+
 while running:
     # poll for events
 
@@ -53,20 +94,16 @@ while running:
     readKey()
     setDirection()
 
-    if direction=="RIGHT":
-        snake_position[0]+=3
-    if direction=="LEFT":
-        snake_position[0]-=3
-    if direction=="UP":
-        snake_position[1]-=3
-    if direction=="DOWN":
-        snake_position[1]+=3
+    updateSnakePosition()
+
+    
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
     pygame.draw.rect(screen, green, pygame.Rect(snake_position[0], snake_position[1], 10,10))
 
-    
+    for wallType in wall.variations:
+        drawWall(wallType, [40,120 ])
     
 
     # RENDER YOUR GAME HERE
@@ -75,6 +112,6 @@ while running:
     pygame.display.update()
     # pygame.display.flip()
 
-    fps.tick(30)  # limits FPS to 60
+    fps.tick(60)  # limits FPS to 60
 
 pygame.quit()
